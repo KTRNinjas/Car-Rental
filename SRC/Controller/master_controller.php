86e32dfb389fa3163c2b23a6e00629a5fb6af0cf
rajnaig1 <rajnaigabesz@gmail.com>
2022-07-21 05:17:55 +0200

    basic project setup for routing IMPORTANT check everyone's KTRNINJA name

---
+++ SRC/Controller/master_controller.php
@@ -0,0 +1,3 @@
+<?php 
+include("Routing/routing.php");
+?>
---
+++ SRC/View/testimplementation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Regisztráció</title>
+</head>
+<body>
+  <div>
+    <form action="" method="POST">
+      <input type="text" name="name">User név</input>
+      <input type="email" name="mail">E-mail</input>
+      <input type="text" name="pass">Password</input>
+      <button type="submit" name="submit">Regisztál</button>
+    </form>
+  </div>
+  <?php 
+  include("../Controller/testimplemetation_controller.php");
+  init($_POST);
+?>
+</body>
+</html>
