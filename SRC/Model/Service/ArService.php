92f3d5d9c29b13f1d3d0786b7ec32bbdf8bd67c6
unknown <attilatoaso@yahoo.com>
2022-07-30 20:57:23 +0300

    Atnevezes

---
+++ SRC/Model/Service/ArService.php
@@ -0,0 +1,15 @@
+<?php 
+$path = dirname(__DIR__, 1);
+    include_once($path."/DAO/ArDAO.php");
+    function Feltolto($kapcsolat){
+        return FajtaFeltolto($kapcsolat);
+    }
+    function Kategoria($kapcsolat){
+        return KategoriaFeltolto($kapcsolat);
+    }
+    function KornyezetVedelem($kapcsolat){
+        return KornyezetVedelemFeltolto($kapcsolat);
+    }
+    function AutotipusAdatAtvevo($marka,$tipus,$fajta,$kategoria,$premium,$kornyezetvedelem){
+       return AutoTipusTarolo($marka,$tipus,$fajta,$kategoria,$premium,$kornyezetvedelem);
+    }
