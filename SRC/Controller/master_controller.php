8c4578fe0c912608701a7238ad08bf868dc1f92e
rajnaig1 <109238551+rajnaig1@users.noreply.github.com>
2022-08-01 17:06:03 +0200

    30 mint felhasználó azt akarom hogy legyen routing a projektben azért hogy ne kelljen a mappaszerkezetben kóvályognom (#44)
    * basic project setup for routing IMPORTANT check everyone's KTRNINJA name
    
    * A routing kialakítása két testimplemetációra a get requesteken kívül működőképes
    
    * get request ellenőrizve, működik
    
    * routing átmozgatva az src-re
    
    * nehany fuggveny, hogy a sajat php fileokbol meg lehessen adni routing cimet
    
    * routing két tesztimplementációra működőképes
    
    * minor bug in indexcontroller fixed
    
    * installing and uninstalling routing added
    
    * 404-es honlap
    
    * 404 es access denied homepage kész, access denied meg nem responsive
    
    * Home page és homecontroller megcsinálva
    
    * php
    
    * unit and integration tests for project routing
    
    * pulling gitignore from master
    
    * DIRECTORY_SEPARATOR applied after all $path concatenations
    
    * Elvileg windowsosítva van, ennél jobb tippem nincs

---
+++ SRC/Controller/master_controller.php
@@ -0,0 +1,5 @@
+<?php
+require_once("indexcontroller.php");
+require_once("testimplemetation_controller.php");
+require_once("testimplementation_controller2.php");
+require_once("home_controller.php");
