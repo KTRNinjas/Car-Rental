c5dd8b0042121df988beb09299bc005b39f943c7
unknown <attilatoaso@yahoo.com>
2022-08-02 18:31:51 +0300

    elso

---
+++ SRC/View/Foniindex.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Föni oldala</title>
+</head>
+<body>
+    <form action="" method="POST">
+
+
+    
+    </form>
+    
+</body>
+</html>
