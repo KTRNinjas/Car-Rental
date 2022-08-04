34caabca0b5da47361466763cd73048a07234cf0
rajnaig1 <109238551+rajnaig1@users.noreply.github.com>
2022-08-04 08:17:07 +0200

    kapcsolat (#52)
    * kapcsolat
    
    * kapcsolatok kigyomlálva, dbconn kétszer berakása kiszedve
    
    Co-authored-by: unknown <attilatoaso@yahoo.com>

--- SRC/Controller/master_controller.php
+++ SRC/Controller/master_controller.php
@@ -4,4 +4,4 @@ require_once("testimplemetation_controller.php");
 require_once("testimplementation_controller2.php");
 require_once("home_controller.php");
 require_once("registration_controller.php");
-
+require_once("AutoTipusFelvevoController.php");
