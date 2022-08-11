245fe8dd93212221cfa8b619300551d2415ada08
unknown <attilatoaso@yahoo.com>
2022-08-11 21:27:58 +0300

    lek;rdezes php adminban megy

--- SRC/Model/DAO/AutoLekredezesDAO.php
+++ SRC/Model/DAO/AutoLekredezesDAO.php
@@ -3,8 +3,23 @@ $path = dirname(__DIR__, 2);
 require_once($path . DIRECTORY_SEPARATOR . "Connection" . DIRECTORY_SEPARATOR . "Dbconn.php");
 
 function GetLekerdezesAutoTipusok(){
- 
-
+    $kapcsolat=$GLOBALS["kapcsolat"];  
+    $sql = "SELECT `id`, `Rendszám`, `Alvázszám`,  `Evjarat`, `Teljesitmeny`, `Biztositasi_dij`, `km` FROM `autokolcsonzo`.`cars`";
+    print $sql;
+    $lekerdezesAutoTipusok=[];
+    $lekerdezesAutoTipusokKulsotomb=[];
+    $result = mysqli_query($kapcsolat, $sql);
+    while ($egysor = mysqli_fetch_array($result)){
+        $lekerdezesAutoTipusok["id"]= $egysor["id"];
+        $lekerdezesAutoTipusok["Rendszám"]= $egysor["Rendszám"];
+        $lekerdezesAutoTipusok["Alvázszám"]= $egysor["Alvázszám"];
+        $lekerdezesAutoTipusok["Evjarat"]= $egysor["Evjarat"];
+        $lekerdezesAutoTipusok["Teljesitmeny"]= $egysor["Teljesitmeny"];
+        $lekerdezesAutoTipusok["Biztositasi_dij"]= $egysor["Biztositasi_dij"];
+        $lekerdezesAutoTipusok["km"]= $egysor["km"];
+        array_push($lekerdezesAutoTipusokKulsotomb,$lekerdezesAutoTipusok);
+    }
+    return $lekerdezesAutoTipusokKulsotomb;
 }
 
 ?>
