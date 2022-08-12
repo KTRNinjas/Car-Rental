13cb177d21cce53ad67c031e0407430a788653ce
unknown <attilatoaso@yahoo.com>
2022-08-12 20:00:18 +0300

    css

--- SRC/AutolekerdezesSQL.php
+++ SRC/AutolekerdezesSQL.php
@@ -1,6 +1,6 @@
 <?php
 function creatSzerzodesTabal($kapcsolat){
-    $sql = "CREATE TABLE `autokolcsonzo`.`szerzodes` (`ID` INT NOT NULL AUTO_INCREMENT , `Szerzodes_szama` INT NOT NULL , `Szerzodes_kezdete` DATE NOT NULL , `Szerzodes_lejarta` DATE NOT NULL , `Szerzodo_neve` INT NOT NULL , `Auto_id` INT NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB";
+    $sql = "CREATE TABLE `autokolcsonzo`.`szerzodes` (`ID` INT NOT NULL AUTO_INCREMENT , `Szerzodes_szama` INT NOT NULL , `Szerzodes_kezdete` DATE NOT NULL , `Szerzodes_lejarta` DATE NOT NULL , `Szerzodo_neve` INT NOT NULL ,  PRIMARY KEY (`ID`)) ENGINE = InnoDB";
     $üzenet = "Az Szerzodes tábla létrehozása";
     Query($kapcsolat, $üzenet, $sql);
 }
--- SRC/View/Autofelvevo.php
+++ SRC/View/Autofelvevo.php
@@ -13,6 +13,7 @@ deleteCarController();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Az autófelvevő honlapja</title>
 </head>
 
