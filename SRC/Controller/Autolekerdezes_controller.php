0dc00423687abbea965bf02926fb6f382fa3a95e
unknown <attilatoaso@yahoo.com>
2022-08-15 12:18:04 +0300

    format document

--- SRC/Controller/Autolekerdezes_controller.php
+++ SRC/Controller/Autolekerdezes_controller.php
@@ -4,7 +4,6 @@ include_once($path . DIRECTORY_SEPARATOR . 'Model' . DIRECTORY_SEPARATOR . 'Serv
 function autolekerdezes()
 {
     AutolekerdezesBody();
-
 }
 
 
@@ -19,37 +18,31 @@ function Autolekerdezesfejlec()
     print "<th><small>Prémium díj</small></th>";
     print "<th><small>Ár</small></th>";
     print "<th><small>Hajtaslanc</small></th>";
-    print "<th><small>Evjarat</small></th>";    
+    print "<th><small>Evjarat</small></th>";
     print "<th><small>Valtotipus</small></th>";
 }
 function AutolekerdezesBody()
 {
-    if(isset($_POST["Lefoglalas"])){
+    if (isset($_POST["Lefoglalas"])) {
         Autolekerdezesfejlec();
-        $kezdoDATE=$_POST["kezdoDATE"];
-        $vegDATE=$_POST["vegDATE"];
-        $GetLekerdezesAutok = GetLekerdezesAutok($kezdoDATE,$vegDATE);
+        $kezdoDATE = $_POST["kezdoDATE"];
+        $vegDATE = $_POST["vegDATE"];
+        $GetLekerdezesAutok = GetLekerdezesAutok($kezdoDATE, $vegDATE);
 
-    for ($i = 0; $i < count($GetLekerdezesAutok); $i++) {
-        print "<tr>";
-        foreach ($GetLekerdezesAutok[$i] as $key => $value) {
-            if($key !="Prémium"){
-            print "<td>" . $value . "</td>";
-            }else{
-                if($value == 1){
-                    print  "<td>Premium</td>";
-                }else{
-                    print  "<td>Nem premium </td>";
+        for ($i = 0; $i < count($GetLekerdezesAutok); $i++) {
+            print "<tr>";
+            foreach ($GetLekerdezesAutok[$i] as $key => $value) {
+                if ($key != "Prémium") {
+                    print "<td>" . $value . "</td>";
+                } else {
+                    if ($value == 1) {
+                        print  "<td>Premium</td>";
+                    } else {
+                        print  "<td>Nem premium </td>";
+                    }
                 }
             }
-            
-            
+            print "</tr>";
         }
-        print "</tr>";
-        }
-
-
     }
-
 }
-
