3c462e9a63c25f6094fca6cc222d196d239b74f1
rajnaig1 <rajnaigabesz@gmail.com>
2022-08-23 23:55:03 +0200

    unnecessary requires removed

--- SRC/View/home.php
+++ SRC/View/home.php
@@ -1,7 +1,6 @@
 <?php
 $path = dirname(__DIR__, 1);
 require_once($path . DIRECTORY_SEPARATOR . "Controller" . DIRECTORY_SEPARATOR . "home_controller.php");
-require_once($path . DIRECTORY_SEPARATOR . "Controller" . DIRECTORY_SEPARATOR . "Lefoglalo_controller.php");
 session_start();
 require_once($path . DIRECTORY_SEPARATOR . "Controller" . DIRECTORY_SEPARATOR . "Autolekerdezes_controller.php");
 $hostname = getenv('HTTP_HOST');
