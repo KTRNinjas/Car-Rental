6618aa2c45936cb611b6e216056e4e3d87dba600
unknown <attilatoaso@yahoo.com>
2022-08-24 16:49:25 +0300

    acceptancia

--- SRC/tests/acceptance/FilldbCest.php
+++ SRC/tests/acceptance/FilldbCest.php
@@ -15,4 +15,54 @@ class FillDBCest
         $I->dontSee('error');
         $I->dontSee('notice');
     }
+    public function test_if_AutotipusSQlmainAndsideTable_message_displayed(AcceptanceTester $I)
+    {
+        $I->amOnPage('http://localhost/KTRNINJAS/Car-Rental/SRC/Filldb.php');
+        $I->see("Sikerült a kapcsolat");
+        $I->see("adatbazis torlése sikeres volt!");
+        $I->see("az adatbázis létrehozása sikeres volt!");
+        $I->see("Az autotipus tabla letrehozasa sikeres volt!");
+        $I->see("Az fajta tabla letrehozasa sikeres volt!");
+        $I->see("Az kategoria tabla letrehozasa sikeres volt!");
+        $I->see("Az környezetvédelmibesorolás tabla letrehozasa sikeres volt!");
+        $I->see("Sikerült a kapcsolat");
+        $I->see("Sikerült a kapcsolat");
+        $I->see("Sikerült a kapcsolat");
+        $I->see("Sikerült a kapcsolat");
+        $I->see("Sikerült a kapcsolat");
+        $I->see("Sikerült a kapcsolat");
+        
+        $I->dontSee('warning');
+        $I->dontSee('error');
+        $I->dontSee('notice');
+        
+    }
+    public function test_if_AutotipusSQlsidetableinit_message_displayed(AcceptanceTester $I)
+    {
+        $I->amOnPage('http://localhost/KTRNINJAS/Car-Rental/SRC/Filldb.php');
+        $I->see("a fajta sikeresen felvetük a Combi elemet sikeres volt!");
+        $I->see("a fajta sikeresen felvetük a Terepjáró elemet sikeres volt!");
+        $I->see("a fajta sikeresen felvetük a Sedan elemet sikeres volt!");
+        $I->see("a fajta sikeresen felvetük a PickUp elemet sikeres volt!");
+        $I->see("a fajta sikeresen felvetük a SUV elemet sikeres volt!");
+        $I->see("a fajta sikeresen felvetük a 4X4 elemet sikeres volt!");
+
+        $I->see("a kategoria sikeresen felvetük a Kis személy elemet sikeres volt!");
+        $I->see("a kategoria sikeresen felvetük a Személy autó elemet sikeres volt!");
+        $I->see("a kategoria sikeresen felvetük a Kis teher elemet sikeres volt!");
+        $I->see("a kategoria sikeresen felvetük a Teher elemet sikeres volt!");
+
+
+        $I->see("a környezetvédelmibesorolás sikeresen felvetük a E1 elemet sikeres volt!");
+        $I->see("a környezetvédelmibesorolás sikeresen felvetük a E2 elemet sikeres volt!");
+        $I->see("a környezetvédelmibesorolás sikeresen felvetük a E3 elemet sikeres volt!");
+        $I->see("a környezetvédelmibesorolás sikeresen felvetük a E4 elemet sikeres volt!");
+        $I->see("a környezetvédelmibesorolás sikeresen felvetük a E5 elemet sikeres volt!");
+        $I->see("a környezetvédelmibesorolás sikeresen felvetük a E6 elemet sikeres volt!");
+                
+        $I->dontSee('warning');
+        $I->dontSee('error');
+        $I->dontSee('notice');
+        
+    }
 }
