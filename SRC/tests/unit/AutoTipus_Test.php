8de017549290ec1268fb961c594892b406041289
NeilTomson <attilatoaso@yahoo.com>
2022-09-30 11:03:17 +0300

    kascad megoldas ?

--- SRC/tests/unit/AutoTipus_Test.php
+++ SRC/tests/unit/AutoTipus_Test.php
@@ -180,91 +180,6 @@ class Autotipus_Data_Integration_Test extends \Codeception\Test\Unit
         $this->assertEquals(6, $kornyezetvedelmibesorolas_collector[5]["ID"]);
         $this->assertEquals("E6", $kornyezetvedelmibesorolas_collector[5]["KörnyezetvédelmiBesorolás"]);
     }//<-kesz
-    public function test_if_cars_table_filled_with_elements()
-    {
-        //given
-        $host = "127.0.0.1";
-        $user = "root";
-        $password = "";
-        $kapcsolat = mysqli_connect($host, $user, $password);
-        $sql = "DELETE FROM `autokolcsonzo`.`cars`";
-        $this->assertTrue(mysqli_query($kapcsolat, $sql));
-        //when
-        fill_testcars($kapcsolat);
-        $sql = "SELECT * FROM `autokolcsonzo`.`cars`";
-        $car_collector = [];
-        $result = mysqli_query($kapcsolat, $sql);
-        while ($egysor = mysqli_fetch_array($result)) {
-            array_push($car_collector, $egysor);
-        }
-        //then
-        $this->assertEquals(5, count($car_collector));
-
-        $this->assertEquals(1, $car_collector[0]["id"]);
-        $this->assertEquals("AAA-001", $car_collector[0]["Rendszám"]);
-        $this->assertEquals("123123123123", $car_collector[0]["Alvázszám"]);
-        $this->assertEquals(1, $car_collector[0]["hajtaslanc_id"]);
-        $this->assertEquals(1, $car_collector[0]["valtotipus_id"]);
-        $this->assertEquals("2008", $car_collector[0]["Evjarat"]);
-        $this->assertEquals(8, $car_collector[0]["Teljesitmeny"]);
-        $this->assertEquals(100000, $car_collector[0]["Biztositasi_dij"]);
-        $this->assertEquals(50000, $car_collector[0]["km"]);
-        $this->assertEquals("2022-07-31", $car_collector[0]["Forgalmi_megujitasanak_ideje"]);
-        $this->assertEquals(1, $car_collector[0]["Autotipus_id"]);
-        $this->assertEquals(NULL, $car_collector[0]["Kivezetve"]);
-
-        $this->assertEquals(2, $car_collector[1]["id"]);
-        $this->assertEquals("BBB-002", $car_collector[1]["Rendszám"]);
-        $this->assertEquals("123123123123", $car_collector[1]["Alvázszám"]);
-        $this->assertEquals(2, $car_collector[1]["hajtaslanc_id"]);
-        $this->assertEquals(2, $car_collector[1]["valtotipus_id"]);
-        $this->assertEquals("2009", $car_collector[1]["Evjarat"]);
-        $this->assertEquals(9, $car_collector[1]["Teljesitmeny"]);
-        $this->assertEquals(110000, $car_collector[1]["Biztositasi_dij"]);
-        $this->assertEquals(60000, $car_collector[1]["km"]);
-        $this->assertEquals("2022-08-31", $car_collector[1]["Forgalmi_megujitasanak_ideje"]);
-        $this->assertEquals(2, $car_collector[1]["Autotipus_id"]);
-        $this->assertEquals(NULL, $car_collector[1]["Kivezetve"]);
-
-        $this->assertEquals(3, $car_collector[2]["id"]);
-        $this->assertEquals("CCC-003", $car_collector[2]["Rendszám"]);
-        $this->assertEquals("123123123123", $car_collector[2]["Alvázszám"]);
-        $this->assertEquals(3, $car_collector[2]["hajtaslanc_id"]);
-        $this->assertEquals(1, $car_collector[2]["valtotipus_id"]);
-        $this->assertEquals("2010", $car_collector[2]["Evjarat"]);
-        $this->assertEquals(10, $car_collector[2]["Teljesitmeny"]);
-        $this->assertEquals(120000, $car_collector[2]["Biztositasi_dij"]);
-        $this->assertEquals(70000, $car_collector[2]["km"]);
-        $this->assertEquals("2022-09-28", $car_collector[2]["Forgalmi_megujitasanak_ideje"]);
-        $this->assertEquals(3, $car_collector[2]["Autotipus_id"]);
-        $this->assertEquals(NULL, $car_collector[2]["Kivezetve"]);
-
-        $this->assertEquals(4, $car_collector[3]["id"]);
-        $this->assertEquals("DDD-004", $car_collector[3]["Rendszám"]);
-        $this->assertEquals("123123123123", $car_collector[3]["Alvázszám"]);
-        $this->assertEquals(1, $car_collector[3]["hajtaslanc_id"]);
-        $this->assertEquals(1, $car_collector[3]["valtotipus_id"]);
-        $this->assertEquals("2011", $car_collector[3]["Evjarat"]);
-        $this->assertEquals(11, $car_collector[3]["Teljesitmeny"]);
-        $this->assertEquals(130000, $car_collector[3]["Biztositasi_dij"]);
-        $this->assertEquals(80000, $car_collector[3]["km"]);
-        $this->assertEquals("2022-10-28", $car_collector[3]["Forgalmi_megujitasanak_ideje"]);
-        $this->assertEquals(1, $car_collector[3]["Autotipus_id"]);
-        $this->assertEquals('2021-09-28', $car_collector[3]["Kivezetve"]);
-
-        $this->assertEquals(5, $car_collector[4]["id"]);
-        $this->assertEquals("EEE-005", $car_collector[4]["Rendszám"]);
-        $this->assertEquals("123123123123", $car_collector[4]["Alvázszám"]);
-        $this->assertEquals(2, $car_collector[4]["hajtaslanc_id"]);
-        $this->assertEquals(2, $car_collector[4]["valtotipus_id"]);
-        $this->assertEquals("2012", $car_collector[4]["Evjarat"]);
-        $this->assertEquals(12, $car_collector[4]["Teljesitmeny"]);
-        $this->assertEquals(140000, $car_collector[4]["Biztositasi_dij"]);
-        $this->assertEquals(90000, $car_collector[4]["km"]);
-        $this->assertEquals("2022-11-28", $car_collector[4]["Forgalmi_megujitasanak_ideje"]);
-        $this->assertEquals(3, $car_collector[4]["Autotipus_id"]);
-        $this->assertEquals('2022-09-28', $car_collector[4]["Kivezetve"]);
-    }
     public function test_if_Autotipus_table_filled_with_elements()
     {
         //given
@@ -307,8 +222,8 @@ class Autotipus_Data_Integration_Test extends \Codeception\Test\Unit
         $this->assertEquals(2, $autotipus_collector[2]["Kategoria_ID"]);
         $this->assertEquals(0, $autotipus_collector[2]["Prémium"]);
         $this->assertEquals(5, $autotipus_collector[2]["Környezetvédelmi_ID"]);
-    }
-    public function test_if_Cars_table_altered()
+    }//<-kesz
+    public function test_if_Cascadolas_table_altered()
     {
         //given
         $host = "127.0.0.1";
@@ -318,15 +233,14 @@ class Autotipus_Data_Integration_Test extends \Codeception\Test\Unit
         //when
         $sql = "DELETE FROM `autokolcsonzo`.`autotipus`";
         $this->assertTrue(mysqli_query($kapcsolat, $sql));
-        $sql = "DELETE FROM `autokolcsonzo`.`cars`";
+        $sql = "DELETE FROM `autokolcsonzo`.`fajta`";
         $this->assertTrue(mysqli_query($kapcsolat, $sql));
-        $result = CarsTablamegvaltoztatasa($kapcsolat);
+        $result = AutotipusTablamegvaltoztatasa($kapcsolat);
         //then
-        $this->assertEquals("A cars tábla kaszkádolása Sikeres volt!", $result);
-        //after
-        fill_testcars($kapcsolat);
-        fill_testAutoTipus($kapcsolat);
-    }
+        $this->assertEquals("Az autotipus tábla környezetvédelemmel kaszádolva sikeres volt!", $result);
+        $this->assertEquals("az autotipus tábla Kategóriával kaszkádolva sikeres volt!", $result);
+        $this->assertEquals("az autotipus tábla Fajtával kaszkádolva sikeres volt!", $result);
+    }//? lehet hibás
     public function test_if_Cars_table_cascaded_for_autotipus()
     {
         //given
