59e772b37271e94cdd7ceda3f0e890cde23f971a
NeilTomson <attilatoaso@yahoo.com>
2022-10-20 12:17:25 +0300

    set up 3

--- SRC/Model/DAO/AutoTipusDAO.php
+++ SRC/Model/DAO/AutoTipusDAO.php
@@ -1,6 +1,53 @@
 <?php
 $path = dirname(__DIR__, 2);
 require_once($path . DIRECTORY_SEPARATOR . "Connection" . DIRECTORY_SEPARATOR . "Dbconn.php");
+//-----
+//lekérdezés
+
+function getAllAutotipus()
+{
+    $kapcsolat = $GLOBALS['kapcsolat'];
+    $autotipusok = [];
+    $today = date("y-m-d");
+    $sql = "SELECT ID,Márka,Tipus,Prémium,
+    (SELECT Fajta_neve FROM `autokolcsonzo`.`fajta` WHERE id=Fajta_ID ) AS fajta,
+    (SELECT Kategoria FROM `autokolcsonzo`.`kategoria` WHERE id=Kategoria_ID ) AS kategoria,
+    (SELECT KörnyezetvédelmiBesorolás FROM `autokolcsonzo`.`környezetvédelmibesorolás` WHERE id=Környezetvédelmi_ID) AS KörnyezetvédelmiBesolas FROM `autokolcsonzo`.`autotipus`";
+    $result = mysqli_query($kapcsolat, $sql);
+    while ($egysor = mysqli_fetch_assoc($result)) {
+        $autotipus = [];
+        foreach ($egysor as $key => $value) {
+            
+                $autotipus[$key] = $value;
+            
+        }
+        array_push($autotipusok, $autotipus);
+    }
+    return $autotipusok;
+}
+
+//update delete 
+function deleteAutotipusDAO($autotipus_ID){
+    
+        $kapcsolat = $GLOBALS['kapcsolat'];
+        $sql = "DELETE FROM `autokolcsonzo`.autotipus WHERE `autokolcsonzo`.`autotipus`.`ID` = $autotipus_ID";
+        $ok = mysqli_query($kapcsolat, $sql);
+        return $ok;
+    
+    
+}
+
+function updateAutotipusDAO($marka,$tipus,$premium,$fajta_ID,$kategoria_ID,$kornyezetvedelem_ID,$autotipus_ID){
+    $kapcsolat = $GLOBALS['kapcsolat'];
+    
+        $sql = "UPDATE `autokolcsonzo`.`autotipus` SET `Márka` = '$marka',`Tipus` = '$tipus',`Prémium` = '$premium',`Fajta_ID` = '$fajta_ID',`Kategoria_ID` = '$kategoria_ID',`Környezetvédelmi_ID` = '$kornyezetvedelem_ID' WHERE `autotipus`.`ID` = '$autotipus_ID' ";
+       
+    
+    $ok = mysqli_query($kapcsolat, $sql);
+    return $ok;
+}
+
+//----
 function AutoTipusTarolo($Marka, $Tipus, $Fajta, $Kategoria, $Premium, $KornyezetvedelmiBesorolas)
 {
     $kapcsolat = $GLOBALS["kapcsolat"];
--- SRC/View/AutoTipusFelvetel.php
+++ SRC/View/AutoTipusFelvetel.php
@@ -1,3 +1,18 @@
+<?php
+$path = dirname(__DIR__, 1);
+include_once($path . DIRECTORY_SEPARATOR . "Controller" . DIRECTORY_SEPARATOR . "AutoTipusFelvevoController.php");
+updateAutotipusController();
+initAutotipusController();
+initAutotipusbekuldes();
+deleteAutotipusController();
+$hostname = getenv('HTTP_HOST');
+$replacedPath = str_ireplace("\\", "/", $path);
+$izé = "//Car-Rental";
+$url = str_ireplace($_SERVER['DOCUMENT_ROOT'], "", $replacedPath);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="hu">
 
@@ -5,13 +20,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autotipus felvétel</title>
-    <?php
-    $path = dirname(__DIR__, 1);
-    include_once($path . DIRECTORY_SEPARATOR . "Controller" . DIRECTORY_SEPARATOR . "AutoTipusFelvevoController.php");
-    ?>
 </head>
 
 <body>
+    <?php
+    printAutotipusInDB();
+
+    ?>
     <form action="" method="POST">
         <label name="marka" for="">
             <input type="text" name="marka" required placeholder="Márka">
@@ -52,6 +67,7 @@
         printresult();
         ?>
     </div>
+
 </body>
 
 </html>
